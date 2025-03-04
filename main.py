9e261728636d37ae187fcbbf6d20a97e2fa8d0a9
arwynstorm88 <hounddog88@gmail.com>
2025-03-04 10:43:10 -0600

    changed classes into groups

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from constants import *
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -17,8 +20,9 @@ def main():
         dt = clock.tick(60) / 1000
 
         screen.fill((0, 0, 0))
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for drawable_object in drawable:
+            drawable_object.draw(screen)
         pygame.display.flip()
 
 
